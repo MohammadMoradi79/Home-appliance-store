29d89907e3beaa1f12f52b31c397d1f831a28999
mohammad_moradi <mo.mo.be.sch@gmail.com>
2023-06-26 11:29:16 +0430

    make views & templates and map urls

--- store/warehouse/views.py
+++ store/warehouse/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.db.models import Max, F, Func
+from warehouse.models import Product, Category
 
-# Create your views here.
+
+def hello_world(request):
+    # product = Product()
+    # product.category = Category(pk=2)
+    # product.title = 'Washing Machin'
+    # product.unit_price = 10
+    # product.save()
+
+    query_set = Product.objects.filter(id__range=(90, 102))
+
+    return render(request, 'hello.html', {'name': 'Mohammad', 'result': list(query_set)})
