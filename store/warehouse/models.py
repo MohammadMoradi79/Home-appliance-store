a84f4dc10f013f9efb53e37e82062a9274ac74e7
mohammad_moradi <mo.mo.be.sch@gmail.com>
2023-06-26 09:32:53 +0430

    Create Category & Product Models

--- store/warehouse/models.py
+++ store/warehouse/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    category = models.ForeignKey('self', null=True, on_delete=models.CASCADE)
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    title = models.CharField(max_length=255)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+    last_update = models.DateTimeField(auto_now=True)
