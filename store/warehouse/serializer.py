1436482d1606c6d569bfef79587d4d6c2703deb2
mohammad_moradi <mo.mo.be.sch@gmail.com>
2023-07-08 12:21:55 +0430

    Create Product Serializer

--- store/warehouse/serializer.py
+++ store/warehouse/serializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Category
+from .models import Category, Product
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -10,3 +10,11 @@ class CategorySerializer(serializers.ModelSerializer):
     def to_representation(self, category):
         self.fields['children'] = CategorySerializer(many=True, read_only=True)
         return super(CategorySerializer, self).to_representation(category)
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model : Product
+        fields = ['category', 'title', 'price']
+
+    price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
