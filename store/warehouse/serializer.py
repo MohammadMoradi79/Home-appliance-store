09dc9bea03bf51850f788507e3eddbd7ba9e2461
mohammad_moradi <mo.mo.be.sch@gmail.com>
2023-07-08 12:42:19 +0430

    Comment Serializer Created

--- store/warehouse/serializer.py
+++ store/warehouse/serializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Category, Product, Customer
+from .models import Category, Product, Customer, Comment
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -24,3 +24,10 @@ class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
         fields = ['first_name', 'last_name', 'email']
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['product', 'customer', 'content', 'placed_at']
+        
