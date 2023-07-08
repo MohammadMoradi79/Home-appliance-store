f296c317573c02ac07d162f32f03baf46b961bb6
mohammad_moradi <mo.mo.be.sch@gmail.com>
2023-07-08 17:16:42 +0430

    Create CategoryViewSet API

--- store/warehouse/views.py
+++ store/warehouse/views.py
@@ -1,13 +1,14 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
+from rest_framework.generics import ListCreateAPIView
 from rest_framework import status
 from .models import Product, Category
 from .serializer import CategorySerializer #, ProductSerializer
 
 
-@api_view()
-def category_list(request):
-    category = Category.objects.filter(parent__isnull=True)
-    serializer = CategorySerializer(category, many=True)
-    return Response(serializer.data)
+class CategoryViewSet(ReadOnlyModelViewSet, ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    
 
