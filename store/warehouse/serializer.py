31e9d8e7cd89d3c82462e897ce9f422d2c9f9f5e
mohammad_moradi <mo.mo.be.sch@gmail.com>
2023-07-17 09:58:13 +0430

    Connect User to CustomerSerializer

--- store/warehouse/serializer.py
+++ store/warehouse/serializer.py
@@ -23,7 +23,7 @@ class ProductSerializer(serializers.ModelSerializer):
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
-        fields = ['first_name', 'last_name', 'email', 'pk']
+        fields = ['id', 'user_id']
 
 
 class CommentSerializer(serializers.ModelSerializer):
